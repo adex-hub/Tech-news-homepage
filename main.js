d601ac35175ba70a1bce50374b72916595bfd5f8
Adeola <68297427+adex-hub@users.noreply.github.com>
2023-03-10 01:25:57 +0100

    Third commit

--- main.js
+++ main.js
@@ -5,13 +5,20 @@ const darkLayer = document.querySelector(".layer");
 const darken = document.querySelector(".darken");
 
 menuBtn.addEventListener("click", () => {
-  menuComponents.classList.add("active");
-  darkLayer.classList.add("darken");
-  document.body.style.overflow = "hidden";
+  menuComponents.style.display = "grid";
+  setTimeout(() => {
+    menuComponents.classList.add("active");
+    darkLayer.classList.add("darken");
+    document.body.style.overflow = "hidden";
+  }, 200);
+
 });
 closeBtn.addEventListener("click", () => {
   menuComponents.classList.remove("active");
   darkLayer.classList.remove("darken");
-  document.body.style.overflowY = "visible";
-  document.body.style.overflowX = "hidden";
+
+  setTimeout(() =>{
+    menuComponents.style.display = "none"; 
+    document.body.style.overflow = "scroll";
+  }, 300)
 });
